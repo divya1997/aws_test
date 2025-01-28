1b47fe5a68b551a5061032306f90776ddf429143
Diyva <divyamehta199707@gmail.com>
2025-01-28 13:16:25 +0530

    Update to proper Amplify Gen 2 structure with auth and data

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,66 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { Construct } from 'constructs';
+import { defineBackend } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend/auth';
+import { defineFunction } from '@aws-amplify/backend/function';
+import { defineData } from '@aws-amplify/backend/data';
 
-export class MessagesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // Create DynamoDB table
-    const messagesTable = new dynamodb.Table(this, 'MessagesTable', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'timestamp', type: dynamodb.AttributeType.NUMBER },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
-
-    // Create Lambda role
-    const lambdaRole = new iam.Role(this, 'MessageHandlerRole', {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-      managedPolicies: [
-        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')
-      ]
-    });
-
-    // Grant DynamoDB permissions
-    messagesTable.grantReadWriteData(lambdaRole);
-
-    // Create Lambda function
-    const messageHandler = new lambda.Function(this, 'MessageHandler', {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset('amplify/lambda'),
-      environment: {
-        TABLE_NAME: messagesTable.tableName,
-      },
-      role: lambdaRole,
-      timeout: cdk.Duration.seconds(30),
-    });
-
-    // Create API Gateway
-    const api = new apigateway.RestApi(this, 'MessagesApi', {
-      defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
-        allowMethods: apigateway.Cors.ALL_METHODS,
-      },
-      deployOptions: {
-        stageName: 'prod',
-      },
-    });
-
-    // Add routes
-    const messages = api.root.addResource('messages');
-    messages.addMethod('GET', new apigateway.LambdaIntegration(messageHandler));
-    messages.addMethod('POST', new apigateway.LambdaIntegration(messageHandler));
-
-    // Output the API URL
-    new cdk.CfnOutput(this, 'ApiUrl', {
-      value: api.url,
-      description: 'API Gateway URL',
-      exportName: 'MessagesApiUrl',
-    });
+const auth = defineAuth({
+  loginWith: {
+    email: true,
+    phone: false,
+    username: false
   }
-}
+});
+
+const data = defineData({
+  schema: 'amplify/data/schema.graphql',
+  authorizationModes: {
+    defaultAuthorizationMode: 'API_KEY',
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30
+    }
+  }
+});
+
+const messageFunction = defineFunction({
+  name: 'messageFunction',
+  entry: 'amplify/message-function/handler.ts',
+  environment: {
+    TABLE_NAME: 'messages'
+  },
+  permissions: {
+    tables: ['messages']
+  }
+});
+
+export const backend = defineBackend({
+  auth,
+  data,
+  messageFunction
+});
